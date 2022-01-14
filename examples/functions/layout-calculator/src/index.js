c07a48de8dbfe9818e2bdfeb9764523a99cbc3d2
sabaturgay <turgaysaba@outlook.com>
2022-01-14 13:29:14 +0100

    add a layout-calculator function

---
+++ examples/functions/layout-calculator/src/index.js
@@ -0,0 +1,22 @@
+const { calculateLayout }  = require('./layoutCalculator')
+
+exports.handler = async (event, context, callback) => {
+  const {
+    nodes,
+    edges,
+    layoutName,
+    boundingBox,
+  } = event.arguments
+  console.log('event', event)
+  const result = await calculateLayout({
+    boundingBox,
+    graph: {
+      edges,
+      nodes,
+    },
+    layoutName,
+  })
+  console.log('result', result)
+  // callback(null, result)
+  return result
+}
